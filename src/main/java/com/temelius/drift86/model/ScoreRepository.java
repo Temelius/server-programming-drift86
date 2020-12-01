80160ec74608b761c7ca69309032df509ed1aee4
Temelius <teme989@gmail.com>
2020-12-01 10:23:38 +0200

    change findAllByUsername

--- src/main/java/com/temelius/drift86/model/ScoreRepository.java
+++ src/main/java/com/temelius/drift86/model/ScoreRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ScoreRepository extends CrudRepository<Score, Long> {
-	List<Score> findAllByUser(String username);
+	List<Score> findAllByUsername(String username);
 	List<Score> findAllByMapOrderByPointsDesc(Map map);
 	List<Score> findAllByVerified(boolean isVerified);
 }
